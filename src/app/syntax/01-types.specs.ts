9f1001d9e22c1ee7f08c49311fbae6e75e4e97ea
Pankaz <pankaj.nuaa@gmail.com>
2025-01-21 14:08:29 -0500

    initial commit

---
+++ src/app/syntax/01-types.specs.ts
@@ -0,0 +1,10 @@
+// describe('Basic Jasmine Tests', () => {
+//   it('can add two numbers', () => {
+//     // const myName = 'pankaz';
+//     let myAge = 89;
+
+//     myAge = 'Old';
+
+//     expect(myAge).toBe('Old');
+//   });
+// });
